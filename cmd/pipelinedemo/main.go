0e63a1058a0ed80e67978ba7acf4b2f18de37c75
breeze-yang <breeze@deshpro.com>
2018-07-30 16:35:33 +0800

    完成pipeline 文件读写的实践

--- cmd/pipelinedemo/main.go
+++ cmd/pipelinedemo/main.go
@@ -3,9 +3,41 @@ package main
 import (
 	"goPractice/pipeline"
 	"fmt"
+	"os"
+	"bufio"
 )
 
-func main() {
+func main(){
+	const filename = "small.in"
+	const n = 100000000
+	file, err := os.Create(filename)
+	if err != nil {
+		panic(err)
+	}
+	defer file.Close()
+	p := pipeline.RandomSource(n)
+	writer := bufio.NewWriter(file)
+	pipeline.WriterSink(writer, p)
+	writer.Flush()
+
+	file, err = os.Open(filename)
+	if err != nil {
+		panic(err)
+	}
+	defer file.Close()
+
+	p = pipeline.ReaderSource(bufio.NewReader(file))
+	count := 0
+	for v := range p {
+		fmt.Println(v)
+		count++
+		if count >= 100 {
+			break
+		}
+	}
+}
+
+func mergeDemo() {
 	arr1 := pipeline.ArraySource(3, 2, 6, 7, 4)
 	arr2 := pipeline.ArraySource(3, 2, 7, 4, 0, 18, 3, 13)
 	p := pipeline.Merge(
